887efc210f63d99605231210c7049598c34b40bd
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-01 23:32:57 +1100

    complete lab

---
+++ app.rb
@@ -0,0 +1,10 @@
+#puts is a built-in Ruby method that outputs a string to the terminal. Equivalent of console.log()
+
+#When we invoke methods in Ruby, the use of parentheses is optional
+#puts("Hello world!") also works
+
+#the puts method always returns nil
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
